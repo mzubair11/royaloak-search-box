a0d6736352439f6046cfd3ac75f10148d8f20dff
mzubair11 <90902654+mzubair11@users.noreply.github.com>
2021-10-27 12:23:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+(function() {
+var id = 'e313b914-34cd-11ec-b38c-0242ac130002';
+var ci_search = document.createElement('script');
+ci_search.type = 'text/javascript';
+ci_search.async = true;
+ci_search.src = 'https://cse.expertrec.com/api/js/ci_common.js?id=' + id;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ci_search, s);
+})();
